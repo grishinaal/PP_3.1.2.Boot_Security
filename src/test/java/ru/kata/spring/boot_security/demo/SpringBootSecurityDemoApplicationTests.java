dbcda1fe7c88e4fe4147759d3b2de44525f4388c
A.Karpushova <grishinaal@yandex.ru>
2023-05-12 15:03:47 +0300

    3.1.2

--- src/test/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
+++ src/test/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
@@ -1,13 +1,17 @@
 package ru.kata.spring.boot_security.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class SpringBootSecurityDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 	}
 
+
 }
+
